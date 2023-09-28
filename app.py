08d1430c4a8f9707ec1d766d6b29433245e2cf60
M. Farhan Rais <mfarhanrais@gmail.com>
2023-09-28 17:26:49 +0800

    caa 28 1726

--- app.py
+++ app.py
@@ -143,12 +143,12 @@ if selected == 'Insights':
     st.markdown(style, unsafe_allow_html = True)
 
     # Embed dashboard
-        # Define HTML content as a string
+    # Define HTML content as a string
     custom_html = """
     <div class='tableauPlaceholder' id='viz1695892652417' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;In&#47;IntermittentFastingandObesity_16956259155130&#47;ConsiderationofIntermittentFasting&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='IntermittentFastingandObesity_16956259155130&#47;ConsiderationofIntermittentFasting' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;In&#47;IntermittentFastingandObesity_16956259155130&#47;ConsiderationofIntermittentFasting&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1695892652417');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
     """
-    # Embed HTML content
-    st.components.v1.html(custom_html)
+    # Embed HTML content into the notebook as an iframe with height that resizes to the window
+    st.components.v1.html(custom_html, height=600)
 
 ### --- 2nd SECTION --- ###
 if selected == 'Analyse User':
